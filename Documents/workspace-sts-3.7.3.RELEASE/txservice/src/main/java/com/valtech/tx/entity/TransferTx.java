15e16af78b0b5e8758031756e9c4a8dd5651e695
Nuthan SML <nuthan.sml@valtech.com>
2022-10-20 17:53:16 +0530

    Transaction Service

---
+++ Documents/workspace-sts-3.7.3.RELEASE/txservice/src/main/java/com/valtech/tx/entity/TransferTx.java
@@ -0,0 +1,39 @@
+package com.valtech.tx.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class TransferTx {
+	
+	@Id
+	private int fromAccount;
+	private int toAccount;
+	
+	
+	
+	public TransferTx() {
+		super();
+	}
+	public TransferTx(int fromAccount, int toAccount) {
+		super();
+		this.fromAccount = fromAccount;
+		this.toAccount = toAccount;
+	}
+	public int getFromAccount() {
+		return fromAccount;
+	}
+	public void setFromAccount(int fromAccount) {
+		this.fromAccount = fromAccount;
+	}
+	public int getToAccount() {
+		return toAccount;
+	}
+	public void setToAccount(int toAccount) {
+		this.toAccount = toAccount;
+	}
+	
+	
+	
+}
